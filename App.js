b0b777e4fa9b87d79312c96192f4c9670ab13b9e
P Nobile <pablonobile99@gmail.com>
2025-05-12 01:20:22 -0300

    archivos y carpetas

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>PAN</Text>
       <StatusBar style="auto" />
+      <Header></Header>
     </View>
   );
 }
---
+++ src/screens/Newgame.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Newgame = () => {
+  return (
+    <View>
+      <Text>Newgame</Text>
+    </View>
+  )
+}
+
+export default Newgame
+
+const styles = StyleSheet.create({})
